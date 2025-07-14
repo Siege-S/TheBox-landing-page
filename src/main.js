9ceeb75a9b1b93202746abc15a601285f229745f
Siege-S <sanchezchristianjoshua@gmail.com>
2025-07-14 18:49:38 +0800

    initital commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const btnMenu = document.getElementById("btn-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+btnMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+  console.log("btn clicked");
+});
+
+console.log(menuLinks);
+menuLinks.forEach((element) => {
+  element.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
